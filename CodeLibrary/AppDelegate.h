186825aee1f2d525843159816025e00bae72134a
JustToBeBetter <lijinzhu@lijinzhudemacbook-pro.local>
2017-04-12 13:58:49 +0800

    Initial Commit

---
+++ CodeLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeLibrary
+//
+//  Created by 李金柱 on 2017/4/12.
+//  Copyright © 2017年 李金柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeLibrary
+//
+//  Created by 李金柱 on 2017/4/12.
+//  Copyright © 2017年 李金柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
